5f6a198973119865d9fc013903d7548fca50710e
me <me@work.com>
2025-04-09 20:20:21 -0400

    Se trabajan metodos de libroController

--- biblioteca/src/main/java/com/example/biblioteca/controller/LibroController.java
+++ biblioteca/src/main/java/com/example/biblioteca/controller/LibroController.java
@@ -3,10 +3,16 @@ package com.example.biblioteca.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.biblioteca.model.LibroModel;
 import com.example.biblioteca.repository.LibroRepository;
 import com.example.biblioteca.service.LibroService;
 
@@ -19,5 +25,25 @@ public class LibroController {
     @GetMapping
     public List<LibroModel> getLibros(){
         return libroService.getLibros();
+    }
+
+    @PostMapping
+    public LibroModel agregarLibro (@RequestBody LibroModel libro){
+        return libroService.saveLibro(libro);
+    }
+
+    @GetMapping("{id}")
+    public LibroModel getLibroPorId(@PathVariable int id){
+        return libroService.getLibroId(id);
+    }
+
+    @PutMapping("{id}")
+    public LibroModel actualizarLibro(@PathVariable int id, @RequestBody LibroModel libro){
+        return libroService.saveLibro(libro);
+    }
+
+    @DeleteMapping("{id}")
+    public String eliminarLibro(@PathVariable int id){
+        return libroService.eliminarLibro(id);
     }
 }
